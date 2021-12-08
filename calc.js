da0d29f289c9fda1df9e81e4eab8af112be86a9d
Nour/Elm <60050784+NourELMDEV@users.noreply.github.com>
2021-12-08 18:47:22 -0500

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,24 @@
+function add(a,b,c) {
+    var a = parseFloat(document.getElementById('num1').value)
+    var b = parseFloat(document.getElementById('num2').value)
+    var c = a + b
+    alert(c)
+}
+function sub(a,b,c) {
+    var a = document.getElementById('sub1').value
+    var b = document.getElementById('sub2').value
+    var c = a - b
+    alert(c)
+}
+function mul(a,b,c) {
+    var a = document.getElementById('t1').value
+    var b = document.getElementById('t2').value
+    var c = a * b
+    alert(c)
+}
+function division(a,b,c) {
+    var a = document.getElementById('d1').value
+    var b = document.getElementById('d2').value
+    var c = a / b
+    alert(c)
+}
